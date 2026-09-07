65dd44178b1c5cad38a213c7bd44e66d9f7b0b51
tkdtg <yoonsh040913@gmail.com>
2026-09-07 10:29:20 +0900

    202302585 윤상현

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-// 학번:
-// 이름:
+// 학번: 202302585
+// 이름: 윤상현
 
 int main(int argc, const char *argv[]) {
     // TODO: 아래 두 줄을 정확히 출력하도록 코드를 작성하세요.
@@ -11,5 +11,8 @@ int main(int argc, const char *argv[]) {
     //   Hello, OOP!
     //   C++ is ready.
 
+    std::cout << "Hello, OOP!\n";
+    std::cout << "C++ is ready.\n";
+    
     return 0;
 }
